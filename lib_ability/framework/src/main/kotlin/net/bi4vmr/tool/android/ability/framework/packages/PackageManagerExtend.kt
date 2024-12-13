24fdbd378ed63a51c9dca19a25ca091fe9af1d6d
BI4VMR <bi4vmr@outlook.com>
2024-12-13 13:00:31 +0800

    [Feature] 1.新增功能。

---
+++ lib_ability/framework/src/main/kotlin/net/bi4vmr/tool/android/ability/framework/packages/PackageManagerExtend.kt
@@ -0,0 +1,14 @@
+package net.bi4vmr.tool.android.ability.framework.packages
+
+import android.content.pm.PackageManager
+
+/**
+ * [PackageManager]的功能扩展类。
+ *
+ * @author bi4vmr@outlook.com
+ * @since 1.0.0
+ */
+object PackageManagerExtend {
+
+    private const val TAG: String = "BaseLib-PackageManagerExtend"
+}
