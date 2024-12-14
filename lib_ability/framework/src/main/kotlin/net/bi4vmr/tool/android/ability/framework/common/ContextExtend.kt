4c44f2b5d16689b83a1c40e4ee78568cc979b654
BI4VMR <bi4vmr@outlook.com>
2024-12-14 21:32:21 +0800

    [Feature] 1.新增功能。

---
+++ lib_ability/framework/src/main/kotlin/net/bi4vmr/tool/android/ability/framework/common/ContextExtend.kt
@@ -0,0 +1,19 @@
+package net.bi4vmr.tool.android.ability.framework.common
+
+import android.content.Context
+
+/**
+ * [Context]的功能扩展类。
+ *
+ * @author bi4vmr@outlook.com
+ * @since 1.0.0
+ */
+object ContextExtend {
+
+    private const val TAG: String = "BaseLib-ContextExtend"
+
+    @JvmStatic
+    fun getApplication() {
+
+    }
+}
