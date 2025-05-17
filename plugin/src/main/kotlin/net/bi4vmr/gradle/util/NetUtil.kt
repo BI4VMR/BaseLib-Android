1d4f8a93577efbc140df252058a2d19cc48fbeb6
BI4VMR <bi4vmr@outlook.com>
2025-05-17 17:05:16 +0800

    [Build] 1.仓库统一使用自定义Plugin管理。

---
+++ plugin/src/main/kotlin/net/bi4vmr/gradle/util/NetUtil.kt
@@ -0,0 +1,27 @@
+package net.bi4vmr.gradle.util
+
+import java.net.InetSocketAddress
+import java.net.Socket
+
+/**
+ * 网络相关工具。
+ *
+ * @author bi4vmr@outlook.com
+ * @since 1.0.0
+ */
+object NetUtil {
+
+    fun scanByTCP(ip: String, port: Int): Boolean {
+        val socket = Socket()
+        try {
+            socket.connect(InetSocketAddress(ip, port), 500)
+            println("TCP connect test success! IP:[$ip] Port:[$port]")
+            return true
+        } catch (e: Exception) {
+            System.err.println("TCP connect test failed! IP:[$ip] Port:[$port] Reason:[${e.message}]")
+            return false
+        } finally {
+            socket.close()
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,124 +4,23 @@
 pluginManagement {
     // 声明Gradle插件仓库
     repositories {
-        // 添加本地私有仓库与代理镜像，无法直连时应当禁用该配置。
-        val hostName: String = java.net.InetAddress.getLocalHost().hostName
-        println("Current host name is [$hostName]")
-        var isInPrivateLAN = false
-        run {
-            java.net.NetworkInterface.getNetworkInterfaces().toList().forEach {
-                it.inetAddresses.toList().forEach { addr ->
-                    if ((addr is java.net.Inet4Address) && (addr.hostAddress.startsWith("172.16."))) {
-                        isInPrivateLAN = true
-                        return@run
-                    }
-                }
-            }
-        }
-        println("Current host in private LAN? [$isInPrivateLAN]")
-
-        if (hostName.startsWith("BI4VMR") && isInPrivateLAN) {
-            println("Current host is in private network, add LAN repositorys.")
-            maven {
-                isAllowInsecureProtocol = true
-                setUrl("http://172.16.5.1:8081/repository/maven-mirror-tencent/")
-            }
-            maven {
-                isAllowInsecureProtocol = true
-                setUrl("http://172.16.5.1:8081/repository/maven-private/")
-            }
-        } else {
-            if (java.net.InetAddress.getByName("192.168.128.1").isReachable(2000)) {
-                println("Current host is not in private network, add VPN repositorys.")
-                maven {
-                    isAllowInsecureProtocol = true
-                    setUrl("http://192.168.128.1:8081/repository/maven-mirror-tencent/")
-                }
-                maven {
-                    isAllowInsecureProtocol = true
-                    setUrl("http://192.168.128.1:8081/repository/maven-private/")
-                }
-            } else {
-                println("Current host is not in private network, add LOCAL repositorys.")
-                mavenLocal()
-            }
-        }
-
-        // 腾讯云仓库镜像：Maven中心仓库+Google+JCenter
-        maven { setUrl("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
-        // 阿里云仓库镜像：Maven中心仓库+JCenter
-        maven { setUrl("https://maven.aliyun.com/repository/public/") }
         // 阿里云仓库镜像：Gradle社区插件
         maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin/") }
+        // 阿里云仓库镜像：Maven中心仓库+JCenter
+        maven { setUrl("https://maven.aliyun.com/repository/public/") }
+        // 阿里云仓库镜像：Google仓库
+        maven { setUrl("https://maven.aliyun.com/repository/google/") }
+        // 腾讯云仓库镜像：Maven中心仓库+Google+JCenter
+        maven { setUrl("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
 
-        mavenCentral()
         gradlePluginPortal()
+        mavenCentral()
+        google()
     }
 }
 
 // 所有模块的依赖配置
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    // 声明Maven组件仓库
-    repositories {
-        // 添加本地私有仓库与代理镜像，无法直连时应当禁用该配置。
-        val hostName: String = java.net.InetAddress.getLocalHost().hostName
-        var isInPrivateLAN = false
-        run {
-            java.net.NetworkInterface.getNetworkInterfaces().toList().forEach {
-                it.inetAddresses.toList().forEach { addr ->
-                    if ((addr is java.net.Inet4Address) && (addr.hostAddress.startsWith("172.16."))) {
-                        isInPrivateLAN = true
-                        return@run
-                    }
-                }
-            }
-        }
-
-        if (hostName.startsWith("BI4VMR") && isInPrivateLAN) {
-            maven {
-                isAllowInsecureProtocol = true
-                setUrl("http://172.16.5.1:8081/repository/maven-mirror-tencent/")
-            }
-            maven {
-                isAllowInsecureProtocol = true
-                setUrl("http://172.16.5.1:8081/repository/maven-jitpack/")
-            }
-            maven {
-                isAllowInsecureProtocol = true
-                setUrl("http://172.16.5.1:8081/repository/maven-private/")
-            }
-        } else {
-            if (java.net.InetAddress.getByName("192.168.128.1").isReachable(2000)) {
-                maven {
-                    isAllowInsecureProtocol = true
-                    setUrl("http://192.168.128.1:8081/repository/maven-mirror-tencent/")
-                }
-                maven {
-                    isAllowInsecureProtocol = true
-                    setUrl("http://192.168.128.1:8081/repository/maven-jitpack/")
-                }
-                maven {
-                    isAllowInsecureProtocol = true
-                    setUrl("http://192.168.128.1:8081/repository/maven-private/")
-                }
-            } else {
-                mavenLocal()
-            }
-        }
-
-        // 腾讯云仓库镜像：Maven中心仓库+Google+JCenter
-        maven { setUrl("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
-        // 阿里云仓库镜像：Maven中心仓库+JCenter
-        maven { setUrl("https://maven.aliyun.com/repository/public/") }
-        // 阿里云仓库镜像：Google
-        maven { setUrl("https://maven.aliyun.com/repository/google/") }
-
-        mavenCentral()
-        google()
-        maven { setUrl("https://jitpack.io/") }
-    }
-
     // 版本管理配置
     versionCatalogs {
         // 公共组件(Java)
@@ -159,22 +58,23 @@ dependencyResolutionManagement {
 /* ----- 工程结构声明 ----- */
 // 主工程名称
 rootProject.name = "BaseLib-Android"
+// 加载自定义插件
+includeBuild("plugin")
 
-
-// ----- 依赖传递 -----
+/* ----- 依赖传递 ----- */
 include(":lib_all")
 
-// ----- 通用组件 -----
+/* ----- 通用组件 ----- */
 // Logcat扩展
 include(":lib_common:logcat")
 
-// ----- 用户界面 -----
+/* ----- 用户界面 ----- */
 // 通用工具
 include(":lib_ui:universal")
 // 模拟时钟
 include(":lib_ui:analogclock")
 
-// ----- 核心能力 -----
+/* ----- 核心能力 ----- */
 // 敏感权限监控工具
 include(":lib_ability:privacymonitor")
 // Framework扩展
@@ -182,7 +82,7 @@ include(":lib_ability:framework")
 // Framework扩展（空实现）
 include(":lib_ability:framework_fakeimplementation")
 
-// ----- 示例程序 -----
+/* ----- 示例程序 ----- */
 // 通用组件 - Logcat扩展
 include(":demo_common:logcat")
 
